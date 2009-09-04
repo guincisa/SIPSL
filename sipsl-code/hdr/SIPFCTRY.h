e97293b637159fc304f558ff45a2dae234270609
guic <guic@8d13bd44-46d8-4671-819c-ea08f47c0839>
2009-09-04 09:33:25 +0000

    Sip factory header

---
+++ sipsl-code/hdr/SIPFCTRY.h
@@ -0,0 +1,33 @@
+//**********************************************************************************
+//**********************************************************************************
+//**********************************************************************************
+// SIPSL Sip Service Layer
+// Copyright (C) 2009 Guglielmo Incisa di Camerana
+//
+//    This program is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//**********************************************************************************
+//**********************************************************************************
+//**********************************************************************************
+
+
+#define SIPFCTRY_H
+
+class SIPFCTRY {
+	private:
+
+	public:
+
+		void createINVITE(MESSAGE* source, MESSAGE* target);
+		void createTRY(MESSAGE* source, MESSAGE* target);
+};
