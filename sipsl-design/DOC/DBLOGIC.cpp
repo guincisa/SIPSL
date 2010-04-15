5e9d49fe71f2a466fda65cdac1a3910f675d8087
guic <guic@8d13bd44-46d8-4671-819c-ea08f47c0839>
2010-04-15 16:12:08 +0000

    multitier sipsl

--- sipsl-design/DOC/DBLOGIC.cpp
+++ sipsl-design/DOC/DBLOGIC.cpp
@@ -13,19 +13,7 @@
 
 using namespace std;
 
-//pthread_rwlock_t rwlock;
-
-int readwaiting = 0;
-pthread_mutex_t conreadwaitingmutex;
-pthread_cond_t c_readwaiting;
-
-pthread_mutex_t r1;
-pthread_mutex_t r2;
-pthread_mutex_t r3;
-pthread_mutex_t r4;
-pthread_mutex_t w;
-
-
+pthread_rwlock_t rwlock;
 
 pthread_mutex_t m1;
 pthread_mutex_t m2;
@@ -50,61 +38,29 @@ void* read_d1(void*){
 	sleep_time.tv_sec = 5;
 	sleep_time.tv_nsec = 0;
 
-	int get_lock;
 
 	while(true){
-		get_lock = 0;
 		pthread_mutex_lock(&m1);
 
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH start read 1" << endl;
 
-		while (get_lock == 0){
-			if (pthread_mutex_trylock(&r1) != EBUSY) {
-				get_lock = 1;
-			}
-			else if (pthread_mutex_trylock(&r2) != EBUSY) {
-				get_lock = 2;
-			}
-			else if (pthread_mutex_trylock(&r3) != EBUSY) {
-				get_lock = 3;
-			}
-			else if (pthread_mutex_trylock(&r4) != EBUSY) {
-				get_lock = 4;
-			}
-			if (get_lock == 0){
-				cout<<"here"<<endl;
-				pthread_mutex_lock(&conreadwaitingmutex);
-				readwaiting ++;
-				pthread_cond_wait(&c_readwaiting,&conreadwaitingmutex);
-				readwaiting --;
-				pthread_mutex_unlock(&conreadwaitingmutex);
-
-			}
-		}
+		int rc = pthread_rwlock_rdlock(&rwlock);
 
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH reading on 1" << endl;
 
 		nanosleep(&sleep_time,NULL);
+
+		rc = pthread_rwlock_unlock(&rwlock);
+
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH reading off 1" << endl;
-
-		if (get_lock == 1)
-			pthread_mutex_unlock(&r1);
-		else if (get_lock == 2)
-			pthread_mutex_unlock(&r2);
-		else if (get_lock == 3)
-			pthread_mutex_unlock(&r3);
-		else if (get_lock == 4)
-			pthread_mutex_unlock(&r4);
-		pthread_cond_signal(&c_readwaiting);
 
 
 	}
 
 }
-
 void* read_d2(void*){
 
 	SysTime mytime;
@@ -113,59 +69,31 @@ void* read_d2(void*){
 	sleep_time.tv_sec = 5;
 	sleep_time.tv_nsec = 0;
 
-	int get_lock;
 
 	while(true){
-		get_lock = 0;
 		pthread_mutex_lock(&m2);
 
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH start read 2" << endl;
 
-		while (get_lock == 0){
-			if (pthread_mutex_trylock(&r1) != EBUSY) {
-				get_lock = 1;
-			}
-			else if (pthread_mutex_trylock(&r2) != EBUSY) {
-				get_lock = 2;
-			}
-			else if (pthread_mutex_trylock(&r3) != EBUSY) {
-				get_lock = 3;
-			}
-			else if (pthread_mutex_trylock(&r4) != EBUSY) {
-				get_lock = 4;
-			}
-			if (get_lock == 0){
-				pthread_mutex_lock(&conreadwaitingmutex);
-				readwaiting ++;
-				pthread_cond_wait(&c_readwaiting,&conreadwaitingmutex);
-				readwaiting --;
-				pthread_mutex_unlock(&conreadwaitingmutex);
-
-			}
-		}
+		int rc = pthread_rwlock_rdlock(&rwlock);
 
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH reading on 2" << endl;
 
 		nanosleep(&sleep_time,NULL);
+
+		rc = pthread_rwlock_unlock(&rwlock);
+
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH reading off 2" << endl;
-
-		if (get_lock == 1)
-			pthread_mutex_unlock(&r1);
-		else if (get_lock == 2)
-			pthread_mutex_unlock(&r2);
-		else if (get_lock == 3)
-			pthread_mutex_unlock(&r3);
-		else if (get_lock == 4)
-			pthread_mutex_unlock(&r4);
-		pthread_cond_signal(&c_readwaiting);
 
 
 	}
 
-}void* read_d3(void*){
+}
+
+void* read_d3(void*){
 
 	SysTime mytime;
 
@@ -173,59 +101,31 @@ void* read_d2(void*){
 	sleep_time.tv_sec = 5;
 	sleep_time.tv_nsec = 0;
 
-	int get_lock;
 
 	while(true){
-		get_lock = 0;
 		pthread_mutex_lock(&m3);
 
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH start read 3" << endl;
 
-		while (get_lock == 0){
-			if (pthread_mutex_trylock(&r1) != EBUSY) {
-				get_lock = 1;
-			}
-			else if (pthread_mutex_trylock(&r2) != EBUSY) {
-				get_lock = 2;
-			}
-			else if (pthread_mutex_trylock(&r3) != EBUSY) {
-				get_lock = 3;
-			}
-			else if (pthread_mutex_trylock(&r4) != EBUSY) {
-				get_lock = 4;
-			}
-			if (get_lock == 0){
-				pthread_mutex_lock(&conreadwaitingmutex);
-				readwaiting ++;
-				pthread_cond_wait(&c_readwaiting,&conreadwaitingmutex);
-				readwaiting --;
-				pthread_mutex_unlock(&conreadwaitingmutex);
-
-			}
-		}
+		int rc = pthread_rwlock_rdlock(&rwlock);
 
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH reading on 3" << endl;
 
 		nanosleep(&sleep_time,NULL);
+
+		rc = pthread_rwlock_unlock(&rwlock);
+
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH reading off 3" << endl;
-
-		if (get_lock == 1)
-			pthread_mutex_unlock(&r1);
-		else if (get_lock == 2)
-			pthread_mutex_unlock(&r2);
-		else if (get_lock == 3)
-			pthread_mutex_unlock(&r3);
-		else if (get_lock == 4)
-			pthread_mutex_unlock(&r4);
-		pthread_cond_signal(&c_readwaiting);
 
 
 	}
 
-}void* read_d4(void*){
+}
+
+void* read_d4(void*){
 
 	SysTime mytime;
 
@@ -233,54 +133,24 @@ void* read_d2(void*){
 	sleep_time.tv_sec = 5;
 	sleep_time.tv_nsec = 0;
 
-	int get_lock;
 
 	while(true){
-		get_lock = 0;
 		pthread_mutex_lock(&m4);
 
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH start read 4" << endl;
 
-		while (get_lock == 0){
-			if (pthread_mutex_trylock(&r1) != EBUSY) {
-				get_lock = 1;
-			}
-			else if (pthread_mutex_trylock(&r2) != EBUSY) {
-				get_lock = 2;
-			}
-			else if (pthread_mutex_trylock(&r3) != EBUSY) {
-				get_lock = 3;
-			}
-			else if (pthread_mutex_trylock(&r4) != EBUSY) {
-				get_lock = 4;
-			}
-			if (get_lock == 0){
-				pthread_mutex_lock(&conreadwaitingmutex);
-				readwaiting ++;
-				pthread_cond_wait(&c_readwaiting,&conreadwaitingmutex);
-				readwaiting --;
-				pthread_mutex_unlock(&conreadwaitingmutex);
-
-			}
-		}
+		int rc = pthread_rwlock_rdlock(&rwlock);
 
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH reading on 4" << endl;
 
 		nanosleep(&sleep_time,NULL);
+
+		rc = pthread_rwlock_unlock(&rwlock);
+
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH reading off 4" << endl;
-
-		if (get_lock == 1)
-			pthread_mutex_unlock(&r1);
-		else if (get_lock == 2)
-			pthread_mutex_unlock(&r2);
-		else if (get_lock == 3)
-			pthread_mutex_unlock(&r3);
-		else if (get_lock == 4)
-			pthread_mutex_unlock(&r4);
-		pthread_cond_signal(&c_readwaiting);
 
 
 	}
@@ -294,84 +164,27 @@ void *write_d1(void*){
 	sleep_time.tv_sec = 10;
 	sleep_time.tv_nsec = 0;
 
-	bool got1 = false;
-	bool got2 = false;
-	bool got3 = false;
-	bool got4 = false;
 
 	while(true){
-
-		got1 = false;
-		got2 = false;
-		got3 = false;
-		got4 = false;
-
 		pthread_mutex_lock(&m5);
 
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH start write 1" << endl;
 
-		pthread_mutex_lock(&w);
-
-		while ((got1== false) && (got2== false) && (got3== false) && (got4== false)) {
-			if (!got1){
-				int rc = pthread_mutex_trylock(&r1);
-				if (rc == EBUSY)
-					got1 = false;
-				else
-					got1 = true;
-			}
-			if (!got2){
-				int rc = pthread_mutex_trylock(&r2);
-				if (rc == EBUSY)
-					got2 = false;
-				else
-					got2 = true;
-
-			}
-			if (!got3){
-				int rc = pthread_mutex_trylock(&r3);
-				if (rc == EBUSY)
-					got3 = false;
-				else
-					got3 = true;
-
-
-			}
-			if (!got4){
-				int rc = pthread_mutex_trylock(&r4);
-				if (rc == EBUSY)
-					got4 = false;
-				else
-					got4 = true;
-
-			}
-		}
-
-
-
+		int rc = pthread_rwlock_wrlock(&rwlock);
 
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH writing on 1" << endl;
 
 		nanosleep(&sleep_time,NULL);
 
+		rc = pthread_rwlock_unlock(&rwlock);
+
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH writing off 1" << endl;
-
-		pthread_mutex_unlock(&r1);
-		pthread_mutex_unlock(&r2);
-		pthread_mutex_unlock(&r3);
-		pthread_mutex_unlock(&r4);
-		pthread_mutex_unlock(&w);
-		pthread_cond_signal(&c_readwaiting);
-
-
-
 	}
 
 }
-
 void *write_d2(void*){
 	SysTime mytime;
 
@@ -379,83 +192,28 @@ void *write_d2(void*){
 	sleep_time.tv_sec = 10;
 	sleep_time.tv_nsec = 0;
 
-	bool got1 = false;
-	bool got2 = false;
-	bool got3 = false;
-	bool got4 = false;
 
 	while(true){
-
-		got1 = false;
-		got2 = false;
-		got3 = false;
-		got4 = false;
-
 		pthread_mutex_lock(&m6);
 
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH start write 2" << endl;
 
-		pthread_mutex_lock(&w);
-
-		while ((got1== false) && (got2== false) && (got3== false) && (got4== false)) {
-			if (!got1){
-				int rc = pthread_mutex_trylock(&r1);
-				if (rc == EBUSY)
-					got1 = false;
-				else
-					got1 = true;
-			}
-			if (!got2){
-				int rc = pthread_mutex_trylock(&r2);
-				if (rc == EBUSY)
-					got2 = false;
-				else
-					got2 = true;
-
-			}
-			if (!got3){
-				int rc = pthread_mutex_trylock(&r3);
-				if (rc == EBUSY)
-					got3 = false;
-				else
-					got3 = true;
-
-
-			}
-			if (!got4){
-				int rc = pthread_mutex_trylock(&r4);
-				if (rc == EBUSY)
-					got4 = false;
-				else
-					got4 = true;
-
-			}
-		}
-
-
-
+		int rc = pthread_rwlock_wrlock(&rwlock);
 
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH writing on 2" << endl;
 
 		nanosleep(&sleep_time,NULL);
 
+		rc = pthread_rwlock_unlock(&rwlock);
+
 		gettimeofday(&mytime.tv, &mytime.tz);
 		cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " TH writing off 2" << endl;
-
-		pthread_mutex_unlock(&r1);
-		pthread_mutex_unlock(&r2);
-		pthread_mutex_unlock(&r3);
-		pthread_mutex_unlock(&r4);
-		pthread_mutex_unlock(&w);
-		pthread_cond_signal(&c_readwaiting);
-
-
-
 	}
 
 }
+
 
 
 int main() {
@@ -468,7 +226,7 @@ int main() {
 
 	SysTime mytime;
 
-	//rc = pthread_rwlock_init(&rwlock, NULL);
+	rc = pthread_rwlock_init(&rwlock, NULL);
 
 	pthread_mutex_lock(&m1);
 	pthread_mutex_lock(&m2);
@@ -490,111 +248,111 @@ int main() {
     //cout << (unsigned long)mytime.tv.tv_sec*1000000+mytime.tv.tv_usec - (unsigned long)progtime.tv.tv_sec*1000000+progtime.tv.tv_usec << " MAIN Ready" << endl;
     cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000)<< " MAIN Ready" << endl;
 
-//    //////////////////////////////////////////////////////////////////////////////
-//    //test parallel read
-//    //read 1
-//    gettimeofday(&mytime.tv, &mytime.tz);
-//    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 1" << endl;
-//	pthread_mutex_unlock(&m1);
-//
-//	//read 2
-//	sleep_time.tv_sec = 2;
-//	sleep_time.tv_nsec = 0;
-//	nanosleep(&sleep_time,NULL);
-//    gettimeofday(&mytime.tv, &mytime.tz);
-//    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 2" << endl;
-//	pthread_mutex_unlock(&m2);
-//
-//	//read 3
-//	sleep_time.tv_sec = 2;
-//	sleep_time.tv_nsec = 0;
-//	nanosleep(&sleep_time,NULL);
-//    gettimeofday(&mytime.tv, &mytime.tz);
-//    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 2" << endl;
-//	pthread_mutex_unlock(&m3);
-//
-//	//read 4
-//	sleep_time.tv_sec = 2;
-//	sleep_time.tv_nsec = 0;
-//	nanosleep(&sleep_time,NULL);
-//    gettimeofday(&mytime.tv, &mytime.tz);
-//    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 2" << endl;
-//	pthread_mutex_unlock(&m4);
-//
-//
-//    gettimeofday(&mytime.tv, &mytime.tz);
-//    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN finish" << endl;
-//
-//
-//	sleep_time.tv_sec = 10;
-//	sleep_time.tv_nsec = 0;
-//	nanosleep(&sleep_time,NULL);
-//
-//    //////////////////////////////////////////////////////////////////////////////
-//    //test write
-//    //write 1
-//    cout << endl;
-//    gettimeofday(&mytime.tv, &mytime.tz);
-//    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN write 1" << endl;
-//	pthread_mutex_unlock(&m5);
-//
-//	//write 2
-//	sleep_time.tv_sec = 2;
-//	sleep_time.tv_nsec = 0;
-//	nanosleep(&sleep_time,NULL);
-//    gettimeofday(&mytime.tv, &mytime.tz);
-//    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN write 2" << endl;
-//	pthread_mutex_unlock(&m6);
-//
-//	sleep_time.tv_sec = 20;
-//	sleep_time.tv_nsec = 0;
-//	nanosleep(&sleep_time,NULL);
-//
-//    //////////////////////////////////////////////////////////////////////////////
-//    //test write and read
-//    //write 1
-//    cout << endl;
-//    gettimeofday(&mytime.tv, &mytime.tz);
-//    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN write 1" << endl;
-//	pthread_mutex_unlock(&m5);
-//
-//	//read 1
-//	sleep_time.tv_sec = 2;
-//	sleep_time.tv_nsec = 0;
-//	nanosleep(&sleep_time,NULL);
-//    gettimeofday(&mytime.tv, &mytime.tz);
-//    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 1" << endl;
-//	pthread_mutex_unlock(&m1);
-//
-//	//read 2
-//	sleep_time.tv_sec = 2;
-//	sleep_time.tv_nsec = 0;
-//	nanosleep(&sleep_time,NULL);
-//    gettimeofday(&mytime.tv, &mytime.tz);
-//    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 2" << endl;
-//	pthread_mutex_unlock(&m2);
-//
-//	//read 3
-//	sleep_time.tv_sec = 2;
-//	sleep_time.tv_nsec = 0;
-//	nanosleep(&sleep_time,NULL);
-//    gettimeofday(&mytime.tv, &mytime.tz);
-//    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 3" << endl;
-//	pthread_mutex_unlock(&m3);
-//
-//	//read 4
-//	sleep_time.tv_sec = 2;
-//	sleep_time.tv_nsec = 0;
-//	nanosleep(&sleep_time,NULL);
-//    gettimeofday(&mytime.tv, &mytime.tz);
-//    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 4" << endl;
-//	pthread_mutex_unlock(&m4);
-//
-//	//fin qui tutto ok
-//
-//	sleep_time.tv_sec = 40;
-//	sleep_time.tv_nsec = 0;
-//	nanosleep(&sleep_time,NULL);
+    //////////////////////////////////////////////////////////////////////////////
+    //test parallel read
+    //read 1
+    gettimeofday(&mytime.tv, &mytime.tz);
+    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 1" << endl;
+	pthread_mutex_unlock(&m1);
+
+	//read 2
+	sleep_time.tv_sec = 2;
+	sleep_time.tv_nsec = 0;
+	nanosleep(&sleep_time,NULL);
+    gettimeofday(&mytime.tv, &mytime.tz);
+    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 2" << endl;
+	pthread_mutex_unlock(&m2);
+
+	//read 3
+	sleep_time.tv_sec = 2;
+	sleep_time.tv_nsec = 0;
+	nanosleep(&sleep_time,NULL);
+    gettimeofday(&mytime.tv, &mytime.tz);
+    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 2" << endl;
+	pthread_mutex_unlock(&m3);
+
+	//read 4
+	sleep_time.tv_sec = 2;
+	sleep_time.tv_nsec = 0;
+	nanosleep(&sleep_time,NULL);
+    gettimeofday(&mytime.tv, &mytime.tz);
+    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 2" << endl;
+	pthread_mutex_unlock(&m4);
+
+
+    gettimeofday(&mytime.tv, &mytime.tz);
+    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN finish" << endl;
+
+
+	sleep_time.tv_sec = 10;
+	sleep_time.tv_nsec = 0;
+	nanosleep(&sleep_time,NULL);
+
+    //////////////////////////////////////////////////////////////////////////////
+    //test write
+    //write 1
+    cout << endl;
+    gettimeofday(&mytime.tv, &mytime.tz);
+    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN write 1" << endl;
+	pthread_mutex_unlock(&m5);
+
+	//write 2
+	sleep_time.tv_sec = 2;
+	sleep_time.tv_nsec = 0;
+	nanosleep(&sleep_time,NULL);
+    gettimeofday(&mytime.tv, &mytime.tz);
+    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN write 2" << endl;
+	pthread_mutex_unlock(&m6);
+
+	sleep_time.tv_sec = 20;
+	sleep_time.tv_nsec = 0;
+	nanosleep(&sleep_time,NULL);
+
+    //////////////////////////////////////////////////////////////////////////////
+    //test write and read
+    //write 1
+    cout << endl;
+    gettimeofday(&mytime.tv, &mytime.tz);
+    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN write 1" << endl;
+	pthread_mutex_unlock(&m5);
+
+	//read 1
+	sleep_time.tv_sec = 2;
+	sleep_time.tv_nsec = 0;
+	nanosleep(&sleep_time,NULL);
+    gettimeofday(&mytime.tv, &mytime.tz);
+    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 1" << endl;
+	pthread_mutex_unlock(&m1);
+
+	//read 2
+	sleep_time.tv_sec = 2;
+	sleep_time.tv_nsec = 0;
+	nanosleep(&sleep_time,NULL);
+    gettimeofday(&mytime.tv, &mytime.tz);
+    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 2" << endl;
+	pthread_mutex_unlock(&m2);
+
+	//read 3
+	sleep_time.tv_sec = 2;
+	sleep_time.tv_nsec = 0;
+	nanosleep(&sleep_time,NULL);
+    gettimeofday(&mytime.tv, &mytime.tz);
+    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 3" << endl;
+	pthread_mutex_unlock(&m3);
+
+	//read 4
+	sleep_time.tv_sec = 2;
+	sleep_time.tv_nsec = 0;
+	nanosleep(&sleep_time,NULL);
+    gettimeofday(&mytime.tv, &mytime.tz);
+    cout << (unsigned long)(mytime.tv.tv_sec*1000 - progtime.tv.tv_sec*1000) << " MAIN read 4" << endl;
+	pthread_mutex_unlock(&m4);
+
+	//fin qui tutto ok
+
+	sleep_time.tv_sec = 40;
+	sleep_time.tv_nsec = 0;
+	nanosleep(&sleep_time,NULL);
 
     //////////////////////////////////////////////////////////////////////////////
     //test write and read and write
