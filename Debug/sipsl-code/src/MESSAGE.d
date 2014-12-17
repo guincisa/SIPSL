e27da1cfe607ff231498fe9cfb71ea2e022016d1
guic <guic@8d13bd44-46d8-4671-819c-ea08f47c0839>
2014-12-17 11:37:43 +0000

    test

--- Debug/sipsl-code/src/MESSAGE.d
+++ Debug/sipsl-code/src/MESSAGE.d
@@ -1,16 +1,16 @@
 sipsl-code/src/MESSAGE.d: ../sipsl-code/src/MESSAGE.cpp \
- /home/guic/DEV/wscpp/SIPSL_SVN/sipsl-code/hdr/UTIL.h \
- /home/guic/DEV/wscpp/SIPSL_SVN/sipsl-code/hdr/CS_HEADERS.h \
- /home/guic/DEV/wscpp/SIPSL_SVN/sipsl-code/hdr/CALL_OSET.h \
- /home/guic/DEV/wscpp/SIPSL_SVN/sipsl-code/hdr/MESSAGE.h \
- /home/guic/DEV/wscpp/SIPSL_SVN/sipsl-code/hdr/SUDP.h
+ /home/guic/DEV/SIPSL/SIPSL_SVN_2/sipsl-code/hdr/UTIL.h \
+ /home/guic/DEV/SIPSL/SIPSL_SVN_2/sipsl-code/hdr/CS_HEADERS.h \
+ /home/guic/DEV/SIPSL/SIPSL_SVN_2/sipsl-code/hdr/CALL_OSET.h \
+ /home/guic/DEV/SIPSL/SIPSL_SVN_2/sipsl-code/hdr/MESSAGE.h \
+ /home/guic/DEV/SIPSL/SIPSL_SVN_2/sipsl-code/hdr/SUDP.h
 
-/home/guic/DEV/wscpp/SIPSL_SVN/sipsl-code/hdr/UTIL.h:
+/home/guic/DEV/SIPSL/SIPSL_SVN_2/sipsl-code/hdr/UTIL.h:
 
-/home/guic/DEV/wscpp/SIPSL_SVN/sipsl-code/hdr/CS_HEADERS.h:
+/home/guic/DEV/SIPSL/SIPSL_SVN_2/sipsl-code/hdr/CS_HEADERS.h:
 
-/home/guic/DEV/wscpp/SIPSL_SVN/sipsl-code/hdr/CALL_OSET.h:
+/home/guic/DEV/SIPSL/SIPSL_SVN_2/sipsl-code/hdr/CALL_OSET.h:
 
-/home/guic/DEV/wscpp/SIPSL_SVN/sipsl-code/hdr/MESSAGE.h:
+/home/guic/DEV/SIPSL/SIPSL_SVN_2/sipsl-code/hdr/MESSAGE.h:
 
-/home/guic/DEV/wscpp/SIPSL_SVN/sipsl-code/hdr/SUDP.h:
+/home/guic/DEV/SIPSL/SIPSL_SVN_2/sipsl-code/hdr/SUDP.h:
