a4072af1d0f6860c153db1663def74c0e9a89f4f
guic <guic@8d13bd44-46d8-4671-819c-ea08f47c0839>
2017-04-17 22:30:03 +0000

    new code for server

--- SIPSL_TCP/src/server.cpp
+++ SIPSL_TCP/src/server.cpp
@@ -1,72 +1,109 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netdb.h>
+#include<arpa/inet.h>
 
-#include <netdb.h>
-#include <netinet/in.h>
 
-#include <string.h>
+void * get_in_addr(struct sockaddr * sa)
+{
+        if(sa->sa_family == AF_INET)
+        {
+                return &(((struct sockaddr_in *)sa)->sin_addr);
+        }
 
-int main( int argc, char *argv[] ) {
-   int sockfd, newsockfd, portno, clilen;
-   char buffer[256];
-   struct sockaddr_in serv_addr, cli_addr;
-   int  n;
+        return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
 
-   /* First call to socket() function */
-   sockfd = socket(AF_INET, SOCK_STREAM, 0);
+int main(int argc, char * argv[])
+{
+        // Variables for writing a server.
+        /*
+        1. Getting the address data structure.
+        2. Openning a new socket.
+        3. Bind to the socket.
+        4. Listen to the socket.
+        5. Accept Connection.
+        6. Receive Data.
+        7. Close Connection.
+        */
+        int status;
+        struct addrinfo hints, * res;
+        int listner;
 
-   if (sockfd < 0) {
-      perror("ERROR opening socket");
-      exit(1);
-   }
 
-   /* Initialize socket structure */
-   bzero((char *) &serv_addr, sizeof(serv_addr));
-   portno = 5001;
+        // Before using hint you have to make sure that the data structure is empty
+        memset(& hints, 0, sizeof hints);
+        // Set the attribute for hint
+        hints.ai_family = AF_UNSPEC; // We don't care V4 AF_INET or 6 AF_INET6
+        hints.ai_socktype = SOCK_STREAM; // TCP Socket SOCK_DGRAM
+        hints.ai_flags = AI_PASSIVE;
 
-   serv_addr.sin_family = AF_INET;
-   serv_addr.sin_addr.s_addr = INADDR_ANY;
-   serv_addr.sin_port = htons(portno);
+        // Fill the res data structure and make sure that the results make sense.
+        status = getaddrinfo(NULL, "8888" , &hints, &res);
+        if(status != 0)
+        {
+                fprintf(stderr,"getaddrinfo error: %s\n",gai_strerror(status));
+        }
 
-   /* Now bind the host address using bind() call.*/
-   if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-      perror("ERROR on binding");
-      exit(1);
-   }
+        // Create Socket and check if error occured afterwards
+        listner = socket(res->ai_family,res->ai_socktype, res->ai_protocol);
+        if(listner < 0 )
+        {
+                fprintf(stderr,"socket error: %s\n",gai_strerror(status));
+        }
 
-   /* Now start listening for the clients, here process will
-      * go in sleep mode and will wait for the incoming connection
-   */
+        // Bind the socket to the address of my local machine and port number
+        status = bind(listner, res->ai_addr, res->ai_addrlen);
+        if(status < 0)
+        {
+                fprintf(stderr,"bind: %s\n",gai_strerror(status));
+        }
 
-   listen(sockfd,5);
-   clilen = sizeof(cli_addr);
+        status = listen(listner, 10);
+        if(status < 0)
+        {
+                fprintf(stderr,"listen: %s\n",gai_strerror(status));
+        }
 
-   /* Accept actual connection from the client */
-   newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+        // Free the res linked list after we are done with it
+        freeaddrinfo(res);
 
-   if (newsockfd < 0) {
-      perror("ERROR on accept");
-      exit(1);
-   }
 
-   /* If connection is established then start communicating */
-   bzero(buffer,256);
-   n = read( newsockfd,buffer,255 );
+        // We should wait now for a connection to accept
+        int new_conn_fd;
+        struct sockaddr_storage client_addr;
+        socklen_t addr_size;
+        char s[INET6_ADDRSTRLEN]; // an empty string
 
-   if (n < 0) {
-      perror("ERROR reading from socket");
-      exit(1);
-   }
+        // Calculate the size of the data structure
+        addr_size = sizeof client_addr;
 
-   printf("Here is the message: %s\n",buffer);
+        printf("I am now accepting connections ...\n");
 
-   /* Write a response to the client */
-   n = write(newsockfd,"I got your message",18);
+        while(1){
+                // Accept a new connection and return back the socket desciptor
+                new_conn_fd = accept(listner, (struct sockaddr *) & client_addr, &addr_size);
+                if(new_conn_fd < 0)
+                {
+                        fprintf(stderr,"accept: %s\n",gai_strerror(new_conn_fd));
+                        continue;
+                }
 
-   if (n < 0) {
-      perror("ERROR writing to socket");
-      exit(1);
-   }
+                inet_ntop(client_addr.ss_family, get_in_addr((struct sockaddr *) &client_addr),s ,sizeof s);
+                printf("I am now connected to %s \n",s);
+                status = send(new_conn_fd,"Welcome", 7,0);
+                if(status == -1)
+                {
+                        close(new_conn_fd);
+                        _exit(4);
+                }
 
-   return 0;
+        }
+        // Close the socket before we finish
+        close(new_conn_fd);
+
+        return 0;
 }
